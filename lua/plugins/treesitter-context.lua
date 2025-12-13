e56ba1495468a345a5c1a4902d540074303f213d
Isaac Linder <ilinder@speedeondata.com>
2025-12-13 08:51:38 -0600

    merging

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -3,3 +3,12 @@ return {
   opts = { mode = "cursor", max_lines = 4 },
 }
 
+  config = function(_, opts)
+    require("treesitter-context").setup(opts)
+
+    -- Make the sticky header visually distinct
+    vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "#2e3440" })      -- main line
+    vim.api.nvim_set_hl(0, "TreesitterContextLineNumber", { bg = "#2e3440" })
+    vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "#5e81ac" })
+  end,
+}
